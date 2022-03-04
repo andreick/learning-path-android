5ce325f849fdd55ce1ee8ab32fca8a08c44dee4d
Andreick <andreick.gomes.ismart@gmail.com>
2022-03-04 20:38:17 -0300

    CoroutinesRoom: Room setup

---
+++ Demos/Coroutines/CoroutinesRoom/app/src/main/java/com/example/coroutinesroom/model/User.kt
@@ -0,0 +1,14 @@
+package com.example.coroutinesroom.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    val username: String,
+    @ColumnInfo(name = "password_hash") val passwordHash: Int,
+    val info: String
+) {
+    @PrimaryKey(autoGenerate = true) val id: Long = 0
+}
