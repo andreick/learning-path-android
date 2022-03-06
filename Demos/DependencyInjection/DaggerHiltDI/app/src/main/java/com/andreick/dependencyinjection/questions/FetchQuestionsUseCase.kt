6a37705261049a2f823a3f1a21bb20ce20cfb55c
Andreick <andreick.gomes.ismart@gmail.com>
2022-03-06 14:35:43 -0300

    DaggerHiltDi: extract Domain logic from QuestionsListActivity to FetchQuestionsUseCase

---
+++ Demos/DependencyInjection/DaggerHiltDI/app/src/main/java/com/andreick/dependencyinjection/questions/FetchQuestionsUseCase.kt
@@ -0,0 +1,43 @@
+package com.andreick.dependencyinjection.questions
+
+import com.andreick.dependencyinjection.Constants
+import com.andreick.dependencyinjection.networking.StackoverflowApi
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object FetchQuestionsUseCase {
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+
+    suspend fun fetchLatestQuestions(): Result {
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = stackoverflowApi.lastActiveQuestions(20)
+                if (response.isSuccessful && response.body() != null) {
+                    return@withContext Result.Success(response.body()!!.questions)
+                } else {
+                    return@withContext Result.Failure
+                }
+            } catch (t: Throwable) {
+                if (t !is CancellationException) {
+                    return@withContext Result.Failure
+                } else {
+                    throw t
+                }
+            }
+        }
+    }
+
+    sealed class Result {
+        class Success(val questions: List<Question>) : Result()
+        object Failure : Result()
+    }
+}
--- Demos/DependencyInjection/DaggerHiltDI/app/src/main/java/com/andreick/dependencyinjection/screens/questionslist/QuestionsListActivity.kt
+++ Demos/DependencyInjection/DaggerHiltDI/app/src/main/java/com/andreick/dependencyinjection/screens/questionslist/QuestionsListActivity.kt
@@ -2,21 +2,17 @@ package com.andreick.dependencyinjection.screens.questionslist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.andreick.dependencyinjection.Constants
-import com.andreick.dependencyinjection.networking.StackoverflowApi
+import com.andreick.dependencyinjection.questions.FetchQuestionsUseCase
 import com.andreick.dependencyinjection.questions.Question
 import com.andreick.dependencyinjection.screens.common.dialogs.ServerErrorDialogFragment
 import com.andreick.dependencyinjection.screens.questiondetails.QuestionDetailsActivity
 import kotlinx.coroutines.*
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener {
 
     private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
     private lateinit var viewMvc: QuestionsListViewMvc
-    private lateinit var stackoverflowApi: StackoverflowApi
 
     private var isDataLoaded = false
 
@@ -24,13 +20,6 @@ class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener
         super.onCreate(savedInstanceState)
         viewMvc = QuestionsListViewMvc(layoutInflater, null)
         setContentView(viewMvc.rootView)
-
-        // init retrofit
-        val retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        stackoverflowApi = retrofit.create(StackoverflowApi::class.java)
     }
 
     override fun onStart() {
@@ -59,16 +48,14 @@ class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener
         coroutineScope.launch {
             viewMvc.showProgressIndication()
             try {
-                val response = stackoverflowApi.lastActiveQuestions(20)
-                if (response.isSuccessful && response.body() != null) {
-                    viewMvc.bindQuestions(response.body()!!.questions)
-                    isDataLoaded = true
-                } else {
-                    onFetchFailed()
-                }
-            } catch (t: Throwable) {
-                if (t !is CancellationException) {
-                    onFetchFailed()
+                when (val result = FetchQuestionsUseCase.fetchLatestQuestions()) {
+                    is FetchQuestionsUseCase.Result.Success -> {
+                        viewMvc.bindQuestions(result.questions)
+                        isDataLoaded = true
+                    }
+                    is FetchQuestionsUseCase.Result.Failure -> {
+                        onFetchFailed()
+                    }
                 }
             } finally {
                 viewMvc.hideProgressIndication()
