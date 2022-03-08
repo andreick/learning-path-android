4168ea1e401dfdee4a1e9a8c4c9e9e4f6c7858b2
Andreick <andreick.gomes.ismart@gmail.com>
2022-03-08 02:07:43 -0300

    DaggerHiltDI: add BaseDialog and DI infrastructure for dialogs

--- Demos/DependencyInjection/DaggerHiltDI/app/src/main/java/com/andreick/dependencyinjection/screens/common/dialogs/ServerErrorDialogFragment.kt
+++ Demos/DependencyInjection/DaggerHiltDI/app/src/main/java/com/andreick/dependencyinjection/screens/common/dialogs/ServerErrorDialogFragment.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.andreick.dependencyinjection.R
 
-class ServerErrorDialogFragment : DialogFragment() {
+class ServerErrorDialogFragment : BaseDialog() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(activity).let {
